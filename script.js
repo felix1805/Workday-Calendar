08b190c39b625bca34bf3c2ab08bf672971c95d0
felix1805 <felix1805@gmail.com>
2022-03-03 17:50:01 -0500

    created and modified script.js to add site functionality.

---
+++ script.js
@@ -0,0 +1,28 @@
+var curr = moment().add(1, "hours").hour();
+moment().format("MMM Do YY");
+var currentDay = document.getElementById("currentDay");
+var currentTime = moment().format("LLL");
+currentDay.textContent += currentTime;
+$("#currentDay").text(moment().format("LLL"));
+$(".row").each(function () {
+  var hour = $(this).data("hour");
+  var message = localStorage.getItem(hour);
+  $(this.find(".description").text(message));
+  $this.addClass(hour > curr ? "future" : hour < curr ? "past" : "present");
+});
+$(".row").on("click", ".saveBtn", function () {
+  var result = $(this).parent().find(".descripion").val();
+  localStorage.setItem($(this).parent().data("hour"), result);
+});
+
+var textArea = document.querySelector(".description")
+var saveBtn = document.querySelector(".saveBtn")
+var inputValue = textArea.value
+
+textArea.value = JSON.parse(localStorage.getItem("inputValue"))
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault()
+  localStorage.setItem("inputValue", JSON.stringify(textArea.value));
+  renderMessage()
+})
+
