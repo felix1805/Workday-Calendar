ca93bbca5ee537829fbcbc95cb6dc8f3ff25f8ee
felix1805 <felix1805@gmail.com>
2022-03-04 15:22:26 -0500

    added comments and cleared local storage before final submission.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// code for current time and date to display at top of page.
 var curr = moment().add(1, "hours").hour();
 moment().format("MMM Do YY");
 var currentDay = document.getElementById("currentDay");
@@ -14,39 +15,16 @@ $(".row").each(function () {
   $(this).addClass(hour > curr ? "future" : hour < curr ? "past" : "present");
 });
 
-
-//$(".row").on("click", ".saveBtn", function () {
- // console.log('WE GOT CLICKED!!!')
-  //var result = $(this).children().find(".descripion").val();
-
- // console.log('this shoudl b the text', result)
- // localStorage.setItem($(this).parent().data("hour"), result);
-//});
-
+// function and variables for local storage to obtain and maintain values after refresh.
 var textArea = document.querySelector(".description")
 var saveBtn = document.querySelector(".saveBtn")
-
-
 var inputValue = textArea.value
-
-//function textArea() {
-// var textContent = {
-//    textArea: textArea.value
-// };
-// localStorage.setItem("textContent", JSON.stringify(textContent));
-//}
-// saveBtn.addEventListener('click', function(event) {
-//   event.preventDefault()
-//   localStorage.setItem('hello', 'JSON.stringify(textArea.value');
-// })
-
-// textArea.value = JSON.parse(localStorage.getItem("inputValue"))
 containerEl[0].addEventListener("click", function (event) {
   if (event.target.matches(".saveBtn")) {
     localStorage.setItem(
       event.target.parentNode.dataset.hour,
       JSON.stringify(
-        event.target.parentNode.querySelector(".description").value
+        event.target.parentNode.querySelector('.description').value
       )
     );
   }
